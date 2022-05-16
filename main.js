229bcffbadde1a2d7cf5c6e697fbef092f2b974f
Brian OrihuelaP <borihuelap001@alumno.uaemex.mx>
2022-05-15 19:12:53 -0500

    Agregando version final 1.0 y extinciones html

---
+++ main.js
@@ -0,0 +1,135 @@
+const Fish = [{
+        id: 0,
+        photo: "https://www.elplural.com/uploads/s1/89/13/56/european-mink-at-osnabrueck-zoo-02-cropped.jpeg",
+        name: "Visón marino",
+        scientificName: "Mustela macrodon",
+        region: "América del Norte",
+        Feeding: "Carnivora",
+        weight: "pesaban entre 70 y 110 kg",
+        yearExtinction: 1870,
+        decription: "El visón marino fue descrito por primera vez en 1903, tras su extinción; la información con respecto su apariencia externa y hábitos se deriva de especulación y en cuentas hechas por comerciantes de pieles y nativos americanos. Puede haber exhibido un comportamiento similar al visón americano, por ejemplo probablemente practicaba poliginandria, eraterritorial y tenía una dieta similar, pero orientado más hacia el mar. Probablemente se encontraba en las costas de Nueva Inglaterra y las provincias marítimas de Canadá, pero su expansión puede haber sido más hacia el sur durante el último periodo glacial. No obstante, su área de ocupación puede haber estado restringida solamente a las costas de Nueva Inglaterra, específicamente el golfo de Maine, o solamente a islas cercanas. El más grande de los visones, el visón marino, fue muy codiciado por los comerciantes de pieles y ello causó su extinción a finales de 1800 o a principios de 1900.",
+    },
+    {
+        id: 1,
+        name: "león marino del Japón",
+        scientificName: "Zalophus japonicus",
+        region: "Habitaba ambientes marinos y costeros del archipiélago del Japón",
+        Feeding: "Carnivora",
+        weight: "pesaban entre 450 y 560 kg",
+        yearExtinction: 1974,
+        decription: "Esta especie presentaba un notorio dimorfismo sexual, ya que los machos adultos tenían una cresta sagital pronunciada, un color gris oscuro,una longitud de 230 a 250 cm de largo, mientras que las hembras eran notablemente menores, con una longitud de 164 cm, y un color gris más claro. Tenían un cuerpo aerodinámico, con una capa de grasa debajo de la piel, para proporcionar calor y flotabilidad. Poseían ojos grandes que les ayudan a compensar los niveles bajos de la luz en el ambiente subacuático, mientras que sus bigotes incrementaban su sentido del tacto. Sus ventanas nasales se cerraban automáticamente una vez tocaban el agua. Sus aletas delanteras largas rotaban hacia afuera para un mejor movimiento en tierra, y las propulsan hacia adelante en el agua, donde permanecían el mayor tiempo posible.",
+
+    },
+    {
+        id: 2,
+        name: "Vaca marina de Steller",
+        scientificName: "Hydrodamalis gigas",
+        region: "Mar de Bering",
+        Feeding: "Herbívoro",
+        weight: "pesaba de 4 a 10 toneladas",
+        yearExtinction: 1768,
+        decription: "es una especie extinta de mamífero sirénido de la familia Dugongidae3​ que medía unos 8 metros de longitud (hasta 10 en algunos casos), que presentaba la cola ahorquillada y una áspera piel negra. La piel era tan resistente que podía usarse para revestir el casco de los buques, y la grasa y carne, además de gratos alimentos, se demostraron como potentes remedios contra el escorbuto debido a su riqueza en vitamina C. Las causas de su extinción fueron la demanda humana de su carne, grasa y piel, de gran calidad. al parecer familias enteras compuestas por el macho, hembra y hasta dos pequeños vivían unidas. Debían de ser monógamos y los jóvenes podían nacer en cualquier época del año, pero sobre todo en otoño. ",
+    },
+    {
+        id: 3,
+        name: "Pez greyling de Nueva Zelanda",
+        scientificName: "Prototroctes oxyrhynchus",
+        region: "Nueva Zelanda",
+        Feeding: "Omnívoro",
+        weight: " 30-45 cm",
+        yearExtinction: 2018,
+        decription: "La dentición del pez se compone de un hocico romo y redondeado, con la mandíbula superior que contiene una única fila de dientes largos y romos que están situados muy juntos formando una estructura en forma de peine. La mandíbula inferior, que contiene otra fila única de dientes más pequeños y afilados, es ligeramente más corta que la mandíbula superior y está menos redondeada, formando un punto que encaja justo dentro de la fila de dientes en la mandíbula superior.La dentición y el sistema digestivo del tímalo neozelandés sugiere que el pez se alimentaba tanto de larvas acuáticas como la mosca caddis, como de plantas como las algas que crecían en las rocas de los lechos de los ríos. El tímalo de Nueva Zelanda se distribuyó ampliamente en las islas norte y sur de Nueva Zelanda",
+    },
+    {
+        id: 4,
+        name: "Delfín chino de río",
+        scientificName: "Lipotes vexillifer",
+        region: "río Yangtsé",
+        Feeding: "Carnivoro",
+        weight: "220 kilos",
+        yearExtinction: 2008,
+        decription: "El delfín baiji, cuyo nombre científico es Lipotes vexillifer, era una especie de cetáceo odontoceto (dentado), familia Iniidae (delfines de río). Era conocido también como simplemente delfín chino de río, formaba parte de la fauna del río Yangtze. En esta región la especie era muy querida y adorada al punto de considerarla “Diosa del Yangtze”. Otros apodos son: el Pai-chi, delfín de aleta blanca, de bandera blanca o delfín del Yangtsé. Estos delfines tenían una cara similar a muchas otras especies, pero se caracterizaban por una aleta posterior de forma triangular baja. Esta se ubicaba en la espalda baja más hacia la aleta caudal, otorgandole mayor estabilidad y maniobrabilidad en el agua. Sus narices eran largas y delgadas. La piel era de color gris claro o azulado a diferencia de su abdomen de característico blanco brillante.",
+    },
+    {
+        id: 5,
+        name: "Tortuga Gigante de la Isla Pinta",
+        scientificName: "Chelonoidis abingdonii",
+        region: "isla Pinta",
+        Feeding: "Herbívoro",
+        weight: "88 kg",
+        yearExtinction: 2012,
+        decription: "La tortuga gigante de Pinta está relacionada genéticamente con las especies de San Cristóbal y Española. Su caparazón tenía la forma de una silla de montar. La parte anterior era más estrecha y comprimida, y levemente elevada, mientras que la parte posterior era más ancha y redondeada. Dada su dieta compuesta por la vegetación alta de la isla, su cuello era alargado y podía elevarse notablemente para alcanzar las ramas más elevadas.En el siglo XIX algunos buques o barcos paraban para alimentarse de tortugas, y en buques grandes podían llegar a llevarse cientos. Eran una buena fuente de comida ya que podían vivir hasta un año en las bodegas sin necesidad de alimentos ni agua. La última amenaza a la especie fue la introducción de especies exóticas. Este es un claro ejemplo de cómo una especie introducida puede producir daños enormes tanto a animales como a plantas. Cuando se introdujeron cabras en los años 50 como alternativa a la fuente de alimento que eran las tortugas, estas hicieron estragos en la flora autóctona, e indirectamente en las tortugas, ya que al no tener alimento desaparecieron hasta el punto que solo ha quedado una de estas tortugas, cuando en el pasado eran miles. ",
+    },
+    {
+        id: 6,
+        name: "Alca gigante",
+        scientificName: "Pinguinus impennis",
+        region: "Océano Atlántico",
+        Feeding: "Carnivora",
+        weight: "5 kg",
+        yearExtinction: 1852,
+        decription: "Los ejemplares adultos tenían alrededor de un metro de altura. El plumaje era negro en las alas y la espalda, cuello y cabeza. A los lados de esta destacaban dos manchas blancas del mismo color que el abdomen. Las patas eran oscuras y palmeadas, y el pico, usado para arponear peces bajo el agua, era muy robusto, razón por la cual los escandinavos la conocían como geirfugl o garefowl, que significa 'ave lanza' en español. El rasgo más distintivo de estas aves era su incapacidad para volar, fruto de su adaptación al buceo. Formaban parejas que incubaban un único huevo extraordinariamente grande (13 centímetros de longitud y 400 gramos de peso) sobre los acantilados o las playas durante la época de reproducción.",
+    },
+    {
+        id: 7,
+        name: "Foca monje del Caribe",
+        scientificName: "Neomonachus tropicalis",
+        region: "Mar Caribe",
+        Feeding: "Carnivora",
+        weight: "130 kg",
+        yearExtinction: 1994,
+        decription: "El primer contacto de los europeos con la foca monje del Caribe fue a través de Cristóbal Colón en 1493, que describió a los animales como lobos de mar y notó el interés económico de la especie. Con la llegada de colonos, la foca monje comenzó a ser cazada por su piel, su grasa y también como alimento. Más tarde se generalizó la idea de que esta foca era una amenaza para la conservación de los bancos de peces y se inició una campaña semiorganizada para exterminarla. La foca monje del Caribe medía entre 2.20 y 2.40 metros de longitud. Su pelaje era castaño en todo el cuerpo menos en la barriga que era blanco amarillento. Las crías nacían totalmente negras. Las hembras tenían cuatro glándulas mamarias, en vez de dos como el resto de focas. Los hábitos de reproducción de esta especie son desconocidos, lo único que se sabe es que daban a luz a una cría en torno al mes de diciembre. Estos animales se alimentaban de peces, cefalópodos y crustáceos y eran muy activos, sobre todo desde el amanecer al crepúsculo. Sus únicos depredadores eran los tiburones caribeños y, más tarde, el hombre.",
+    },
+    {
+        id: 8,
+        name: "Mejillón de Alabama",
+        scientificName: "Mytilus edulis",
+        region: "río Mobile de Alabama",
+        Feeding: "Omnívoros",
+        weight: "100 g",
+        yearExtinction: 2006,
+        decription: "Este molusco filtraba el agua contaminada del río, pero los niveles de polución se volvieron tan altos que el animal ya no pudo soportarlos. La desaparición de esta simple criatura reveló verdades más oscuras sobre el agua, que se estaba llenando de productos químicos peligrosos de las fábricas y causando enfermedades fatales en la comunidad principalmente afroestadounidense que vivía junto a ella. Se alimenta del fitoplancton y zooplancton, e incluso de partículas orgánicas en descomposición, que se encuentra en el agua del mar, mediante la filtración de la misma a través de sus branquias. La capacidad de filtración de los mejillones es enorme, bombeando hasta 8 litros por hora. Fueron los romanos los que comenzaron a consumirlos en gran escala, cultivándolos por el sencillo método de recogerlos en sus criaderos y varios a otros emplazamientos para que allí pudieran reproducirse. ",
+    }
+]
+
+
+function clickaction(b) {
+    const id = b.id;
+    const resultado = Fish.filter(pescado => pescado.id == id);
+    const name = resultado.map(pescado => pescado.name);
+    const name2 = 'Nombre: '+ name;
+    const nameScientific = resultado.map(pescado => pescado.scientificName);
+    const nameScientific2 = 'Nombre Cientifico: ' + nameScientific;
+    const region = resultado.map(pescado => pescado.region);
+    const region2 = 'Localización Geografica: '+ region;
+    const Feeding = resultado.map(pescado => pescado.Feeding);
+    const Feeding2 = 'Clasificación: ' + Feeding;
+    const weight = resultado.map(pescado => pescado.weight);
+    const weight2 = 'Peso: ' +weight
+    const Extinction = resultado.map(pescado => pescado.yearExtinction);
+    const Extinction2 = 'El año de extincion:' + Extinction;
+    const descrit = resultado.map(pescado => pescado.decription);
+    const descrit2 = 'Descripción: ' + descrit
+    
+
+
+
+    var resutl = document.getElementById('pescadoname');
+    var nameScien = document.getElementById('pescadonamescient');
+    var fishregion = document.getElementById('pescadoregion');
+    var Feedi = document.getElementById('pescadofeeding');
+    var weig = document.getElementById('pescadoweight');
+    var resutl2 = document.getElementById('pescadoExtinction');
+    var resutl3 = document.getElementById('pescadoDescrip');
+
+
+
+    resutl.innerHTML = name2;
+    nameScien.innerHTML = nameScientific2;
+    fishregion.innerHTML = region2;
+    Feedi.innerHTML = Feeding2;
+    weig.innerHTML = weight2
+    resutl2.innerHTML = Extinction2;
+    resutl3.innerHTML = descrit2;
+}
+
